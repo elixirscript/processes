c9a41a7d206a12abd805350f1e5d3c0a62134e5a
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-01-05 17:26:59 -0600

    Update demo to show framerate

--- src/docs/js/index.js
+++ src/docs/js/index.js
@@ -2,30 +2,6 @@ import '../css/index.css'
 
 import Processes from '../../index.js'
 
-const system = new Processes.ProcessSystem(
-  new Processes.RequestAnimationScheduler()
-)
-
-const rows = 200
-const columns = 50
-
-const maxProcesses = rows * columns
-
-const main = document.getElementById('main')
-
-tableCreate(rows, columns)
-
-for (let i = 0; i < maxProcesses; i++) {
-  const _pid = system.spawn(function*() {
-    while (true) {
-      yield* render(
-        Math.floor(Math.random() * maxProcesses),
-        Math.floor(Math.random() * 10)
-      )
-    }
-  })
-}
-
 function* render(cell, number) {
   let el = document.getElementById(`td-${cell}`)
   const style = 'group' + (number % 6)
@@ -35,6 +11,7 @@ function* render(cell, number) {
 }
 
 function tableCreate(numberOfRows, numberOfColumns) {
+  const main = document.getElementById('main')
   let tbl = document.createElement('table')
   tbl.cellPadding = 0
   tbl.cellSpacing = 0
@@ -59,3 +36,51 @@ function tableCreate(numberOfRows, numberOfColumns) {
   tbl.appendChild(tbdy)
   main.appendChild(tbl)
 }
+
+// from https://www.growingwiththeweb.com/2017/12/fast-simple-js-fps-counter.html
+const times = []
+let fps
+let fpsEl
+
+function refreshLoop() {
+  window.requestAnimationFrame(() => {
+    const now = performance.now()
+    while (times.length > 0 && times[0] <= now - 1000) {
+      times.shift()
+    }
+    times.push(now)
+    fps = times.length
+    if (!fpsEl) {
+      fpsEl = document.getElementById('fps')
+    }
+
+    fpsEl.textContent = fps.toString()
+    refreshLoop()
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  const system = new Processes.ProcessSystem(
+    new Processes.RequestAnimationScheduler()
+  )
+
+  const rows = 200
+  const columns = 50
+
+  const maxProcesses = rows * columns
+
+  refreshLoop()
+
+  tableCreate(rows, columns)
+
+  for (let i = 0; i < maxProcesses; i++) {
+    const _pid = system.spawn(function*() {
+      while (true) {
+        yield* render(
+          Math.floor(Math.random() * maxProcesses),
+          Math.floor(Math.random() * 10)
+        )
+      }
+    })
+  }
+})
