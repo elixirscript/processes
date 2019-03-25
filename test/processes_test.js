8fbd3c6d7fd98d324613355028a9ef6647c3d4a2
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-03-25 08:10:57 -0500

    Fix tests

--- test/processes_test.js
+++ test/processes_test.js
@@ -1,27 +1,27 @@
-import test from 'ava';
-import Processes from '../src/index.js';
+import test from 'ava'
+import Processes from '../src/index'
 
-let system = null;
+let system = null
 
 test.beforeEach(t => {
-  system = new Processes.ProcessSystem();
-});
+  system = new Processes.ProcessSystem()
+})
 
-test('spawn', function* testSpawn(t) {
+test('spawn', function testSpawn(t) {
   const pid = system.spawn(function*() {
-    yield 1;
-  });
+    yield 1
+  })
 
-  t.is(system.list().length, 2);
-  t.is(system.list()[1], pid);
-});
+  t.is(system.list().length, 2)
+  t.is(system.list()[1], pid)
+})
 
-test('spawn_link',function* testSpawnLink(t) {
+test('spawn_link', function testSpawnLink(t) {
   const pid = system.spawn_link(function*() {
-    yield 1;
-  });
+    yield 1
+  })
 
-  t.is(system.list().length, 2);
-  t.true(system.links.get(pid).has(system.list()[0]));
-  t.true(system.links.get(system.list()[0]).has(pid));
-});
+  t.is(system.list().length, 3)
+  t.true(system.links.get(pid).has(system.list()[0]))
+  t.true(system.links.get(system.list()[0]).has(pid))
+})
